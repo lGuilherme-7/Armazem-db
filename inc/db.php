adbbee41fab5709a9d750b1a3765b31b8552759c
Ronald Guilherme Da Silva Cavalcanti <ronaldguilherme044@gmail.com>
2026-02-01 23:53:28 -0300

    Update db.php

--- inc/db.php
+++ inc/db.php
@@ -19,6 +19,7 @@ define('DB_NAME', 'construmax');
 define('DB_USER', 'root');
 define('DB_PASS', ''); // Senha padrão do XAMPP/WAMP
 define('DB_CHARSET', 'utf8mb4');
+define('banco_de_dados', 'construmax'); // 
 define('AMBIENTE', 'desenvolvimento'); // 'desenvolvimento' ou 'producao'
 
 // ============================================
